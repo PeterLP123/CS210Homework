c6164d7e64fd7f99a03b42a153096c4562c8cc93
PeterLP123 <79646627+PeterLP123@users.noreply.github.com>
2022-10-15 12:33:24 +0100

    first commit

---
+++ src/MonteCarlo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MonteCarlo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int sampleSize = sc.nextInt();
+        int testNo = sc.nextInt();
+        sc.close();
+        int success = 0;
+        for(int c = 0; c < 10000; c++){
+            int[] days = new int[365];
+            for (int i = 0; i < sampleSize; i++) {
+                int randDay = rand.nextInt(days.length);
+                days[randDay] = days[randDay] + 1;
+            }
+            for(int i = 0; i < days.length; i++){
+                if(days[i] == testNo){
+                    success++;
+                    break;
+                }
+            }
+        }
+        System.out.println(success);
+    }
+}
