5b936b8ff83bf061009d37182203324f7f60e54d
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-11-21 19:47:59 +0000

    Added Lab Week 9 - Hash Tables

---
+++ src/Dictionary.java
@@ -0,0 +1,53 @@
+import java.io.*;
+public class Dictionary{
+     
+    private String input[]; 
+
+    public Dictionary(){
+        input = load("src/words.txt");  
+    }
+    
+    public int getSize(){
+        return input.length;
+    }
+    
+    public String getWord(int n){
+        return input[n].trim();
+    }
+    
+    private String[] load(String file) {
+        File aFile = new File(file);     
+        StringBuffer contents = new StringBuffer();
+        BufferedReader input = null;
+        try {
+            input = new BufferedReader( new FileReader(aFile) );
+            String line = null; 
+            //int i = 0;
+            while (( line = input.readLine()) != null){
+                contents.append(line);
+                //i++;
+                contents.append(System.getProperty("line.separator"));
+            }
+        }catch (FileNotFoundException ex){
+            System.out.println("Can't find the file - are you sure the file is in this location: "+file);
+            ex.printStackTrace();
+        }catch (IOException ex){
+            System.out.println("Input output exception while processing file");
+            ex.printStackTrace();
+        }finally{
+            try {
+                if (input!= null) {
+                    input.close();
+                }
+            }catch (IOException ex){
+                System.out.println("Input output exception while processing file");
+                ex.printStackTrace();
+            }
+        }
+        String[] array = contents.toString().split("\n");
+        for(String s: array){
+            s.trim();
+        }
+        return array;
+    }
+}
---
+++ src/HashTables.java
@@ -0,0 +1,99 @@
+public class HashTables {
+    public static void main(String [] args){
+        int size = 99877;
+        String arr[] = new String [90000];
+        int count1 = 0;
+        int count2 = 0;
+        Dictionary input = new Dictionary();
+        while(count1 < arr.length){
+            while(true){
+                count2++;
+                if(input.getWord(count2).matches("^[a-z]+$")){
+                    arr[count1] = input.getWord(count2);
+                    break;
+                }
+            }
+            count1++;
+        }
+        String [] finished = fill(size, arr);
+        for(int i = 0; i < finished.length; i++){
+            System.out.println(finished[i]);
+        }
+        HashTable hash1 = new HashTable(finished);
+        for(int i = 0; i < arr.length; i++){
+            find(size, hash1, arr[i]);
+        }
+        System.out.println(hash1.gettotal());
+    }
+
+    public static int find(int size, HashTable mytable, String word){
+       
+        //fill this in so as to minimize collisions
+        //takes in the HashTable object and the word to be found
+        //the only thing you can do with the HashTable object is call "check"
+        //this method should return the slot in the hashtable where the word is
+        
+        long sum = 0;
+        for(int j = 0; j < word.length() && j < 10; j++){     
+            long charno = word.charAt(j);
+            long powers = (long) (charno * Math.pow(27, j));
+            sum += powers;
+        }
+        sum %= size;
+        while(sum < size){
+            if(mytable.check((int)sum, word)){
+                return (int)sum;
+            }
+            sum++;
+        }
+        return 0;
+    }
+   
+    public static String[] fill(int size, String[] array){
+       
+        //takes in the size of the hashtable, and the array of Strings to be placed in the hashtable
+        //this should add all the words into the hashtable using some system
+        //then it should return the hashtable array
+        String[] hashtable = new String[size];
+        for(int i=0;i<array.length;i++){
+            long sum = 0;
+            for(int j = 0; j < array[i].length() && j < 10; j++){     
+                long charno = array[i].charAt(j);
+                long powers = (long) (charno * Math.pow(27, j));
+                sum += powers;
+            }
+            sum %= size;
+            while(sum < size){
+                if(hashtable[(int)sum] == null){
+                    hashtable[(int)sum] = array[i];
+                    break;
+                }
+                sum++;
+            }
+        }
+        return hashtable;
+    }  
+}
+
+class HashTable{
+   
+    private String[] hashTable;
+    private long total=0;
+
+    public HashTable(String[] input){
+        hashTable = input;
+    }
+
+    public boolean check(int slot, String check){
+        if(hashTable[slot].equals(check)){
+            return true;
+        }else{
+            total++;
+            return false;
+        }
+    }
+   
+    public long gettotal(){
+        return total;
+    }
+}
