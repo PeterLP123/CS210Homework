abf55c125f9a7443a6b53d23fb66c997b7c4c73e
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-11-29 17:31:41 +0000

    Renamed for organisation and closed scanner.

--- src/ExamQuestions/Question4_2022A.java
+++ src/ExamQuestions/Question4_2022A.java
@@ -1,12 +1,13 @@
 package ExamQuestions;
 import java.util.Scanner;
-public class Question4 {
+public class Question4_2022A {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         String [] arr = new String [5];
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextLine();
         }
+        sc.close();
         for (int i = 0; i < arr.length - 1; i++){
             for (int j = 0; j < arr.length - i - 1; j++){
                 if (arr[j].length() > arr[j + 1].length()){
