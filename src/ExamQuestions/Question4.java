136a41fdcd99ce8cc3a5cf4bdb8f6e70330b8dc3
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-11-29 17:29:09 +0000

    2022 Autumn Question 4 completed

---
+++ src/ExamQuestions/Question4.java
@@ -0,0 +1,29 @@
+package ExamQuestions;
+import java.util.Scanner;
+public class Question4 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String [] arr = new String [5];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextLine();
+        }
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++){
+                if (arr[j].length() > arr[j + 1].length()){
+                    String temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }else if(arr[j].length() == arr[j+1].length()){
+                    if(arr[j].compareToIgnoreCase(arr[j+1]) > 0){
+                        String temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
