00584ad9f04ac634826661e15e371d3731721c40
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-11-29 18:47:32 +0000

    Added Q1 January 2022

---
+++ src/ExamQuestions/Question1_2022J.java
@@ -0,0 +1,21 @@
+package ExamQuestions;
+public class Question1_2022J {
+    public static void main(String [] args){
+        int num = 1;
+        boolean found = false;
+        while(!found){
+            for(int i = 1; i <= 20; i++){
+                if(num % i != 0){
+                    break;
+                }
+                if(i == 20){
+                    found = true;
+                }
+            }
+            if(!found){
+                num++;
+            }
+        }
+        System.out.println(num);
+    }
+}
