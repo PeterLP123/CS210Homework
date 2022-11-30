b4c07686a658b52ae4d74ac7c29270d787137952
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-11-30 14:18:58 +0000

    Added start of Q2 2020

---
+++ src/ExamQuestions/Question2_2020J.java
@@ -0,0 +1,18 @@
+/*
+Write a Java program that takes in a String array of 10,000
+English words, and sorts them according to the number of
+unique letters they contain, before printing them out in order.
+The words containing the greatest number of unique letters
+should be printed out first, followed by those containing fewer
+unique letters. If two words use the same number of unique
+letters, then they should be sorted lexicographically (i.e. like a
+dictionary).
+
+For example, the word “fantastic” uses 7 unique letters, so it
+should be printed out before “banana”, which uses only 3 unique
+letters.
+Include comments which explain your code clearly.
+
+State the Big O complexity of your program and justify your
+reasoning. 
+*/
