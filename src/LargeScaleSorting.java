4a31f3a556c2db4e339182f1e93ffd6610d18616
Peter Prendergast <79646627+PeterLP123@users.noreply.github.com>
2022-12-13 09:57:23 +0000

    Updated post-lab.

--- src/LargeScaleSorting.java
+++ src/LargeScaleSorting.java
@@ -2,57 +2,44 @@ import java.util.Scanner;
 public class LargeScaleSorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
+        long lowerbound = sc.nextLong();
+        long upperbound = sc.nextLong();
+        long search = sc.nextLong();
         sc.close();
-        long nums [] = new long[(int) (b - a + 1)];
-        long collatz [] = new long[(int) (b - a + 1)];
+        long nums [] = new long[(int) (upperbound - lowerbound + 1)];
+        long collatz [] = new long[(int) (upperbound - lowerbound + 1)];
         for(int i = 0; i < nums.length; i++) {
-            nums[i] = a;
-            collatz[i] = collatzLength(a);
-            a++;
+            nums[i] = lowerbound;
+            collatz[i] = collatzLength(lowerbound);
+            lowerbound++;
         }
-        /*for(int i = 0; i < nums.length; i++){
-            System.out.println(nums[i] + "," + collatz[i]);
-        }*/
         mergeSort(collatz, nums);
-        /*for(int i = 0; i < nums.length; i++){
-            System.out.println(nums[i] + "," + collatz[i]);
-        }*/
-        System.out.println(nums[(int) c - 1]);
+        System.out.println(nums[(int) search - 1]);
     }
 
-    //Collatz Length
     public static long collatzLength (long n) {
         long length = 1;
         while (n != 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
-            } else {
-                n = 3 * n + 1;
-            }
+            n = (n % 2 == 0) ? n/2 : 3 * n + 1;
             length++;
         }
         return length;
     }
 
-    //Merge Sort
-    public static void mergeSort(long[] arr, long[] arr2) {
-        if (arr.length > 1) {
-            long[] leftc = leftHalf(arr);
+    public static void mergeSort(long[] arrc, long[] arr2) {
+        if (arrc.length > 1) {
+            long[] leftc = leftHalf(arrc);
             long[] left = leftHalf(arr2);
-            long[] rightc = rightHalf(arr);
+            long[] rightc = rightHalf(arrc);
             long[] right = rightHalf(arr2);
 
             mergeSort(leftc, left);
             mergeSort(rightc, right);
 
-            merge(arr, arr2, leftc, left, rightc, right);
+            merge(arrc, arr2, leftc, left, rightc, right);
         }
     }
 
-    //left half of array
     public static long[] leftHalf(long[] arr) {
         long size1 = arr.length / 2;
         long[] left = new long[(int)size1];
@@ -62,31 +49,29 @@ public class LargeScaleSorting {
         return left;
     }
 
-    //right half of array
     public static long[] rightHalf(long[] arr) {
-        long size1 = arr.length / 2;
-        long size2 = arr.length - size1;
+        long middle = arr.length / 2;
+        long size2 = arr.length - middle;
         long[] right = new long[(int)size2];
         for (long i = 0; i < size2; i++) {
-            right[(int)i] = arr[(int) (i + size1)];
+            right[(int)i] = arr[(int) (i + middle)];
         }
         return right;
     }
 
-    //merge method
-    public static void merge(long[] result, long [] result2, long[] leftc, long[] left, long[] rightc, long[] right) {
-        long i1 = 0;
-        long i2 = 0;
+    public static void merge(long[] result, long [] result2, long[] leftc, long[] left, long[]  rightc, long[] right) {
+        long il = 0;
+        long ir = 0;
 
         for (long i = 0; i < result.length; i++) {
-            if (i2 >= rightc.length || (i1 < leftc.length && leftc[(int)i1] <= rightc[(int)i2])) {
-                result[(int)i] = leftc[(int)i1];
-                result2[(int)i] = left[(int)i1];
-                i1++;
+            if (ir >= rightc.length || (il < leftc.length && leftc[(int)il] <= rightc[(int)ir])) {
+                result[(int)i] = leftc[(int)il];
+                result2[(int)i] = left[(int)il];
+                il++;
             } else {
-                result[(int)i] = rightc[(int)i2];
-                result2[(int)i] = right[(int)i2];
-                i2++;
+                result[(int)i] = rightc[(int)ir];
+                result2[(int)i] = right[(int)ir];
+                ir++;
             }
         }
     }
